aaf1df14ab36f601410015d9addc01dde9ce5c3a
JT <jstorbit@gmail.com>
2026-01-12 23:10:48 -0700

    Add get employee by ID response

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ app.get('/employees', (req, res) => {
   res.send(employees)
 })
 
+app.get('/employees/:id', (req, res) => {
+  const { id } = req.params;
+  const idNum = Number(id);
+  const foundEmployee = employees.find((employee) => employee.id === idNum)
+
+  if (foundEmployee) {
+    res.send(foundEmployee)
+  } else {
+    res.status(404)
+    res.send(`No employee with ID# ${id}`);
+  }
+})
+
 export default app;
