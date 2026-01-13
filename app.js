a6c36ef528b5e6738c6bf168f19a7825890ef9f9
JT <jstorbit@gmail.com>
2026-01-12 22:57:53 -0700

    Add employees path to list all employees

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import employees from '#db/employees';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello employees!')
+})
+
+app.get('/employees', (req, res) => {
+  res.send(employees)
 })
 
 export default app;
